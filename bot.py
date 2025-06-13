110cf63deed8de3e77ab3d17d5589bb2e4624e41
carlostoek <carlostoek@gmail.com>
2025-06-12 21:49:31 -0600

    feat: add logging middleware

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 from aiogram import Bot, Dispatcher
 from config import settings
 
+from utils.logger import LoggingMiddleware
+
 bot = Bot(token=settings.bot_token)
 dp = Dispatcher()
+dp.update.middleware(LoggingMiddleware())
